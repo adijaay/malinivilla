d6f64c9eccee1ffc5fe8f50e604b908deab5c5b4
adi wijaya swastama <adiwijayaswastama@gmail.com>
2024-09-10 09:40:02 +0700

    init project

---
+++ src/components/navbar.jsx
@@ -0,0 +1,21 @@
+import '../App.css'
+
+function Navbar() {
+
+  return (
+    <>
+      <div className='w-screen h-auto px-8 py-3  flex flex-row justify-between items-center'>
+        <h1 className='text-black text-2xl font-bold'>halo</h1>
+        <div className='flex flex-row gap-8'>
+          <h1>menu</h1>
+          <h1>menu</h1>
+          <h1>menu</h1>
+          <h1>menu</h1>
+        </div>
+      </div>
+      <div></div>
+    </>
+  )
+}
+
+export default Navbar
